e478bf6476a229d9ba5c98409cca1741b23771b7
parasaurolophus <apps@rader.us>
2024-08-04 04:55:50 -0500

    Improve mermaid content

--- src/components/MermaidTest.vue
+++ src/components/MermaidTest.vue
@@ -4,11 +4,24 @@
         <v-card-text>
             <v-theme-provider theme="dark" with-background>
                 <pre class="mermaid">
-                    graph TB
-                        a --> b
-                        b --> d
-                        a --> c
-                        c --> d
+---
+config:
+  theme: dark
+---
+flowchart TD
+    inception -->|iterate| inception
+    inception -->|requirements| architecture
+    architecture -->|iterate| architecture
+    architecture -->|request change| inception
+    architecture -->|specifications<br>designs| development
+    development -->|iterate| development
+    development -->|request change| architecture
+    development -->|commit| test
+    test -->|iterate| test
+    test -->|report bugs| development
+    test -->|approve| operations
+    operations -->|deploy<br>monitor| operations
+    operations -->|report bugs| development
                 </pre>
             </v-theme-provider>
         </v-card-text>
@@ -18,18 +31,22 @@
         <v-card-text>
             <v-theme-provider theme="dark" with-background>
                 <pre class="mermaid">
-                    sequenceDiagram
+---
+config:
+  theme: dark
+---
+sequenceDiagram
 
-                        actor user
-                        participant client
-                        participant server
-                        participant storage
+    actor user
+    participant client
+    participant server
+    participant storage
 
-                        user ->> client: enter credentials
-                        client ->> client: encrypt credentials
-                        client ->> server: authenticate
-                        server ->> server: decrypt credentials
-                        server ->> storage: verify credentials
+    user ->> client: enter credentials
+    client ->> client: encrypt credentials
+    client ->> server: authenticate
+    server ->> server: decrypt credentials
+    server ->> storage: verify credentials
                 </pre>
             </v-theme-provider>
         </v-card-text>
